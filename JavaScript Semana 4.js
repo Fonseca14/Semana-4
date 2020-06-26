496e22ea5d7bd200be3adbd948d5188f38bdc282
Fonseca14 <56729210+Fonseca14@users.noreply.github.com>
2020-06-26 17:32:31 +0100

    Add files via upload

---
+++ JavaScript Semana 4.js
@@ -0,0 +1,82 @@
+
+var Exemplo = ["um", "dois", "tres"];
+
+function criarArray(){
+	alert(exemplo);
+}
+
+
+var exemplo2 = ["um","dois","tres"];
+exemplo2.push("seis");
+
+
+function aumentarArray(){
+	alert(exemplo2);
+}
+
+
+var exemplo3 = ["um","dois","tres"];
+exemplo3.pop();
+
+function removerArray() {
+	alert(exemplo3);
+}
+
+var exemplo4 = ["um","dois","tres"];
+exemplo4.unshift();
+
+function addinicioArray(){
+	alert(exemplo4);
+}
+
+var exemplo45 = ["um","dois","tres","quatro"];
+exemplo4.shift();
+
+function removeinicioArray(){
+	alert(exemplo5);
+}
+
+
+var exemplo6 = ["um","dois","tres"];
+exemplo6.slice();
+
+function copiarArray(){
+	alert(exemplo6);
+}
+
+
+var exemplo7 = ["um","dois","tres"];
+exemplo7.splice(3,3);
+
+function removePosicaoArray(){
+	alert(exemplo7);
+}
+
+
+var exemplo8 = ["um","dois","tres"];
+exemplo8.sort();
+
+function CrescenteArray(){
+	alert(exemplo8);
+}
+
+var exemplo9 = ["um","dois","tres"];
+exemplo9.reverse();
+
+function DecrescenteArray(){
+	alert(exemplo9);
+}
+
+var exemplo10 = ["um","dois","tres"];
+exemplo10.reverse().push("quatro");
+
+function OrdenaAdd(){
+	alert(exemplo10);
+}
+
+var exemplo11 = ["um","dois","tres","quatro"];
+exemplo11.reverse().splice(1,1);
+
+function OrdenaRemv(){
+	alert(exemplo11);
+}
